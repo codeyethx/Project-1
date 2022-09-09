acc56d816b6a9e2811e83a300a8dae000de3553a
codeyethx <98122731+codeyethx@users.noreply.github.com>
2022-09-09 10:49:26 -0400

    Add files via upload

---
+++ Project0.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string code[] = {
+
+"#include <iostream>",
+"#include <string>",
+" ",
+"int main() {",
+"    std::string code[] = {",
+"    ",
+"    };",
+"    for (int index = 0; index < 6; index++)",
+"        std::cout << index[code] << std::endl;",
+"    for (int line = 0; line < 17; line++) {",
+"        std::cout << (char)34 << line[code]",
+"        <<(char)34 << ',' << std::endl;",
+"    }",
+"    for (int next = 6; next < 17; next++)",
+"        std::cout << next[code] << std::endl;",
+"    return 0; ",
+"}",
+    };
+    for (int index = 0; index < 6; index++)
+        std::cout << index[code] << std::endl;
+    for (int line = 0; line < 17; line++) {
+        std::cout << (char)34 << line[code]
+        << (char)34 << ',' << std::endl;
+    }
+    for (int next = 6; next < 17; next++)
+        std::cout << next[code] << std::endl;
+    return 0;
+}
